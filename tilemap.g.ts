38230386d2164ced8533da50a75b6abbdcc0a9dc
Carlos GV <114511873+carlosgarvil@users.noreply.github.com>
2023-02-08 19:27:48 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ tilemap.g.ts
@@ -0,0 +1,58 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "nivel1":
+            case "nivel1":return tiles.createTilemap(hex`1000100009080808080808080808080808080807020f0a0a0a0a0a0a0a0a0a0a0a0a0a0a020b0b0b0b0b0b0b0b0a0b0b0b0b0b06020b0b0b0b0b0b0b0b0a0b0b0b0b0b06020b010b0b0b0b0b0b0a0b0b010b0b06020b0b0b0b0a0a0a0a0a0b0b0b0b0b06020b0b0b0b0a0d0d0d0a0b0b010b0b06020b010b0a0a0d0d0d0a0b0b0b0b0b06020b0b0b0a0d0d0d0d0a0a0a0a0a0b06020b0b0b0a0d0d0d0d0d100d0d0a0b060a0a0a0a0a0a0a0d0d0d0d0d0d0a0b06020e0c0c0c0c0a0a0d110d0d0a0a0b06020b0b0b0b0a0a0d0d0d0d0d0a0b0b06020b0b0b0a0a0d0d0d0d0d0a0a0b0b06020b0b0a0a0d0d0d0d0d0d0a0b0b01060403030a030303030303030303030305`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 2 2 . . . . . . . 
+. . . . . . 2 2 2 . . . . . . . 
+. . . . . 2 2 2 2 . . . . . . . 
+. . . . . 2 2 2 2 2 2 2 2 . . . 
+. . . . . . . 2 2 2 2 2 2 . . . 
+. . . . . . . . 2 2 2 2 . . . . 
+. . . . . . . 2 2 2 2 2 . . . . 
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.castle.tilePath7,sprites.castle.tilePath9,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.castle.tilePath2,sprites.castle.tilePath1,sprites.castle.tilePath5,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tileDarkGrass1,sprites.dungeon.chestClosed,sprites.dungeon.chestOpen,sprites.castle.tileDarkGrass3,sprites.dungeon.stairLarge], TileScale.Sixteen);
+            case "nivel2":
+            case "nivel2":return tiles.createTilemap(hex`1000100001010101010201010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101060303030303030303030303030303030103030303030303030303030303030301030303030303030303030303030303010303030303030303030303030303030103030303030303030303030303030301010101010101040404040404040405`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.greenSwitchUp,sprites.castle.tileDarkGrass1,sprites.dungeon.floorLight1,sprites.dungeon.doorLockedWest,sprites.builtin.coral0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
